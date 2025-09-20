c3665a4738b3fb4df0f198a7890d25b259a7aac1
maggieguo <m5guo@uwaterloo.ca>
2025-09-19 20:02:25 -0400

    fix edge case for multiple instances of a letter

--- src/guesses.py
+++ src/guesses.py
@@ -15,12 +15,24 @@ def validateGuess(guess: str, word: str) -> list[str]:
         mp[word[i]].add(i)
 
     colors = [Color.GREY] * n
+    
+    # edge case: multiple instance of a letter are in guess but fewer 
+    # instances are in word. Soln: do two passes of marking to remove 
+    # correct guesses first, then for each character correctly guessed but not 
+    # in the right position, decrement the count 
+    
+    # First pass: mark all exact matches (GREEN) and remove them from available letters
     for i in range(n): 
         if guess[i] == word[i]: 
             colors[i] = Color.GREEN
             mp[word[i]].remove(i)
-        elif guess[i] in mp:
+    
+    # Second pass: mark misplaced letters (YELLOW) only if they're still available
+    for i in range(n):
+        if colors[i] == Color.GREY and guess[i] in mp and len(mp[guess[i]]) > 0:
             colors[i] = Color.YELLOW
+            # Remove one instance of this letter from available letters
+            mp[guess[i]].pop()
     
     return colors
 
@@ -36,3 +48,4 @@ if __name__ == "__main__":
     # # should return [GREEN, GREY, GREY, YELLOW, GREEN]
     # print(validateGuess("BEARY", "ABBEY"))
     # # should return [YELLOW, YELLOW, YELLOW, GREY, GREEN]
+    print(validateGuess("TESTS", "LATER"))
